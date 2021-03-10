86ce1a8c10bb7ae70394f87ff18717df9a5ee5c6
Cody Thaller <stevencthaller@gmail.com>
2021-03-10 12:07:53 -0800

    react intro

---
+++ React/ReactIntro/client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      <main>
+        <h2>lksajdflasdf</h2>
+        <p>lasjdflkalskjdfjlk</p>
+      </main>
+  );
+}
+
+export default App;
---
+++ React/ReactIntro/client/src/Components/MyHeader.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const MyHeader = () => {
+    return (
+        <header>
+            <h1>Welcome to my page</h1>
+            <h2>My name is Cody</h2>
+        </header>
+    )
+}
+
+export default MyHeader
+
